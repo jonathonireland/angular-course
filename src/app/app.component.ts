aa72c149e77728edbe8e30678eb5c4defe9e0512
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-07-15 14:16:20 -0400

    angular 17 signal effect API 106 of 113 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, ChangeDetectionStrategy, Signal, ChangeDetectorRef, Component, DoCheck, ElementRef, Inject, InjectionToken, Injector, OnInit, QueryList, ViewChild, ViewChildren, signal, computed} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Signal, ChangeDetectorRef, Component, DoCheck, ElementRef, Inject, InjectionToken, Injector, OnInit, QueryList, ViewChild, ViewChildren, signal, computed, effect} from '@angular/core';
 import { NgFor } from '@angular/common';
 
 @Component({
@@ -11,26 +11,21 @@ export class AppComponent  {
 
   counter = signal(0);
 
-  multiplier: number = 0;
-
   derivedCounter = computed(() => {
-    
     const counter = this.counter();
-
-    if(this.multiplier >= 10){
-      
-      return counter * 10;
-
-    } else {
-      return 0;
-    }
+    return counter * 10;
   });
+
+  constructor() {
+    effect(()=>{
+      const counterValue = this.counter();
+      const derivedCounterValue = this.derivedCounter();
+      console.log(`counter: ${counterValue} derived counter: ${derivedCounterValue}`);
+    });
+  }
 
   increment(){
     this.counter.update(val => val + 1);
   }
 
-  incrementMultiplier() {
-    this.multiplier++;
-  }
 }
