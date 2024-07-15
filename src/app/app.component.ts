d585d5c5d4651ce04ab0dc6aa21389f0af9e9ba1
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-07-15 10:27:24 -0400

    angular 17 signals introduction 101 of 113 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,79 +1,26 @@
-import {AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck, ElementRef, Inject, InjectionToken, Injector, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
-import {COURSES} from '../db-data';
-import {Course} from './model/course';
-import {CourseCardComponent} from './courses/course-card/course-card.component';
-import {HighlightedDirective} from './courses/directives/highlighted.directive';
-import {Observable} from 'rxjs';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { CoursesService } from './courses/courses.service';
-import { APP_CONFIG, AppConfig, CONFIG_TOKEN } from './config';
-import { createCustomElement } from '@angular/elements';
-import { CourseTitleComponent } from './course-title/course-title.component';
-import { CourseImageComponent } from './courses/course-image/course-image.component';
+import {AfterViewInit, ChangeDetectionStrategy, Signal, ChangeDetectorRef, Component, DoCheck, ElementRef, Inject, InjectionToken, Injector, OnInit, QueryList, ViewChild, ViewChildren, signal} from '@angular/core';
 import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [
-    CourseCardComponent,
-    CourseImageComponent,
-    NgFor
-  ],
   standalone:true
 })
-export class AppComponent implements OnInit {
+export class AppComponent  {
 
 
-  courses : Course[] = COURSES;
-
-  performPrefetch: boolean = false;
-
-  display: boolean = false;
-
-  coursesTotal = this.courses.length;
+  counter = signal(0);
 
   
   //courses$: Observable<Course[]>;
 
-  constructor(
-    private coursesService: CoursesService,
-    @Inject (CONFIG_TOKEN) private config: AppConfig,
-    private injector: Injector) {
+  constructor() {
   }
 
-  ngOnInit() {
-    const htmlElement = createCustomElement(CourseTitleComponent,{injector:this.injector});
-    customElements.define('course-title',htmlElement);
+  increment(){
+    this.counter.set(this.counter() + 1);
   }
 
-
-  onEditCourse(){
-
-    const course = this.courses[0];
-
-    const newCourse = {
-      ...course,
-      description: 'ngOnChanges'
-    };
-    this.courses[0] = newCourse;
-
-  }
-
-  save(course:Course){
-    this.coursesService.saveCourse(course)
-      .subscribe(
-        () => console.log('Course Saved!')
-      );
-  }
-
-  onPrefetch() {
-    this.performPrefetch = true;
-  }
-
-  onDisplay() {
-    this.display = true;
-  }
 
 }
