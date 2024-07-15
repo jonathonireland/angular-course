96c82fbfa7910ba0498d4a4cd9bedc02e26dec89
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-07-15 13:00:13 -0400

    signals with compute API 104 of 113 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, ChangeDetectionStrategy, Signal, ChangeDetectorRef, Component, DoCheck, ElementRef, Inject, InjectionToken, Injector, OnInit, QueryList, ViewChild, ViewChildren, signal} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Signal, ChangeDetectorRef, Component, DoCheck, ElementRef, Inject, InjectionToken, Injector, OnInit, QueryList, ViewChild, ViewChildren, signal, computed} from '@angular/core';
 import { NgFor } from '@angular/common';
 
 @Component({
@@ -12,30 +12,14 @@ export class AppComponent  {
 
   counter = signal(0);
 
-  course = signal({
-    id:1,
-    title: "Angular for Beginners"
+  derivedCounter = computed(() => {
+    const counter = this.counter();
+
+    return counter * 10;
   });
-
-  courses = signal([
-    "Angular for Beginners",
-    "Reactive Angular Course"
-  ]);
-
-  constructor() {
-  }
 
   increment(){
     this.counter.update(val => val + 1);
-
-    this.course.set(
-      {
-        id: 1,
-        title: "Hello World"
-      }
-    );
-
-    this.courses.update(courses => [...courses, "Angular Core Deep Dive"]);
   }
 
 
