80e3f9b22c804c007efeec49cd6c8e4f048d503f
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-07-15 12:42:39 -0400

    angular signals with objects and arrays 103 of 113 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,30 @@ export class AppComponent  {
 
   counter = signal(0);
 
+  course = signal({
+    id:1,
+    title: "Angular for Beginners"
+  });
+
+  courses = signal([
+    "Angular for Beginners",
+    "Reactive Angular Course"
+  ]);
+
   constructor() {
   }
 
   increment(){
     this.counter.update(val => val + 1);
+
+    this.course.set(
+      {
+        id: 1,
+        title: "Hello World"
+      }
+    );
+
+    this.courses.update(courses => [...courses, "Angular Core Deep Dive"]);
   }
 
 
